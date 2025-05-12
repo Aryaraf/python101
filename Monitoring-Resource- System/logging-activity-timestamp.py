d190281d94ed4469a3c5579d73f913be00cee2ce
Aryaraf <aryaryanakbarfadillah@gmail.com>
2025-05-12 20:16:38 +0700

    refactor: modularize system logging with function and interval delay

--- Monitoring-Resource- System/logging-activity-timestamp.py
+++ Monitoring-Resource- System/logging-activity-timestamp.py
@@ -1,15 +1,17 @@
 import psutil
+import time
 from datetime import datetime
 
-while True:    
+def get_stats():
     cpu = psutil.cpu_percent(interval=1)
     ram = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    time = datetime.now().strftime("%Y-%m-%d")
-        
-    total = (f"\n[{time}] INFO - CPU usage {cpu}%, RAM usage: {ram}%, Disk usage: {disk}%")
-        
+    times = datetime.now().strftime("[%Y-%m-%d]")
+    return f"{times} INFO - CPU usage {cpu}%, RAM usage: {ram}%, Disk usage: {disk}%\n"
+
+while True:    
+    log = get_stats()   
     with open ("logging.log", "a") as f:
-        f.write(total)
-        
-    print(f"[{time}] INFO - CPU usage {cpu}%, RAM usage: {ram}%, Disk usage: {disk}%")
+        f.write(log)
+    print(log.strip())
+    time.sleep(5)
